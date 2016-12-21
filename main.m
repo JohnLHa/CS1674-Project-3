f70c8dce02cf566d38621d4e6b7a3094c0d3d7cf
John Ha <xxdragonvi3tboixx@yahoo.com>
2016-12-21 14:58:18 -0500

    Updated Comments for Clarity

--- main.m
+++ main.m
@@ -1,7 +1,10 @@
 
+%Repeated operations for 6 images
+
 %panda1 image
 panda1 = imread('panda1.jpg');
 imshow(panda1);
+%prepare image by grayscaling, scaling, and converting to double
 grayPanda1 = rgb2gray(panda1);
 imshow(grayPanda1);
 scaledPanda1 = imresize(grayPanda1, .5);
@@ -11,12 +14,11 @@ P1 = double(scaledPanda1);
 
 binranges = 2.^(0:0.5:7);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtP1 = imfilter(P1, F(:,:,i));
     filtP1 = filtP1(:);
-    %responses(:,:,i)=conv2(I,F(:,:,i),'valid');
     subplot(1, 2, 1)
-    %imagesc(responses(:,:,i))
     imagesc(filtP1);
     if i==1
         bincounts = histc(filtP1(:),binranges);
@@ -25,8 +27,6 @@ for i=1:48
     else
         bincounts = histc(filtP1(:), binranges);
         descriptorPanda1 = [descriptorPanda1;  bincounts];
-        %figure
-        %bar(binranges, bincounts, 'histc');
     end
 end
 
@@ -34,12 +34,14 @@ end
 %panda2 image
 panda2 = imread('panda2.jpg');
 imshow(panda2);
+%prepare image by grayscaling, scaling, and converting to double
 grayPanda2 = rgb2gray(panda2);
 imshow(grayPanda2);
 scaledPanda2 = imresize(grayPanda2, .5);
 imshow(scaledPanda2);
 P2 = double(scaledPanda2);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtP2 = imfilter(P2, F(:,:,i));
     filtP2 = filtP2(:);
@@ -62,12 +64,14 @@ end
 %cardinal1 image
 card1 = imread('cardinal1.jpg');
 imshow(card1);
+%prepare image by grayscaling, scaling, and converting to double
 grayCard1 = rgb2gray(card1);
 imshow(grayCard1);
 scaledCard1 = imresize(grayCard1, .5);
 imshow(scaledCard1);
 C1 = double(scaledCard1);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtC1 = imfilter(C1, F(:,:,i));
     filtC1 = filtC1(:);
@@ -89,12 +93,14 @@ end
 %cardinal 2 image
 card2 = imread('cardinal2.jpg');
 imshow(card2);
+%prepare image by grayscaling, scaling, and converting to double
 grayCard2 = rgb2gray(card2);
 imshow(grayCard2);
 scaledCard2 = imresize(grayCard2, .5);
 imshow(scaledCard2);
 C2 = double(scaledCard2);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtC2 = imfilter(C2, F(:,:,i));
     filtC2 = filtC2(:);
@@ -116,12 +122,14 @@ end
 %leopard 1 image
 leo1 = imread('leopard1.jpg');
 imshow(leo1);
+%prepare image by grayscaling, scaling, and converting to double
 grayLeo1 = rgb2gray(leo1);
 imshow(grayLeo1);
 scaledLeo1 = imresize(grayLeo1, .5);
 imshow(scaledLeo1);
 L1 = double(scaledLeo1);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtL1 = imfilter(L1, F(:,:,i));
     filtL1 = filtL1(:);
@@ -143,12 +151,14 @@ end
 %leopard 2 image
 leo2 = imread('leopard2.jpg');
 imshow(leo2);
+%prepare image by grayscaling, scaling, and converting to double
 grayLeo2 = rgb2gray(leo2);
 imshow(grayLeo2);
 scaledLeo2 = imresize(grayLeo2, .5);
 imshow(scaledLeo2);
 L2 = double(scaledLeo2);
 
+%Place image into histogram bin and create a descriptor
 for i=1:48 
     filtL2 = imfilter(L2, F(:,:,i));
     filtL2 = filtL2(:);
